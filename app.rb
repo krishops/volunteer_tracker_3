76c8f1463e3428f7ec3f8f678288d54b58e94813
Kris Hopper <hopperdavis@gmail.com>
2021-07-30 13:58:54 -0700

    add main and new_project views and initial code to app.rb

--- app.rb
+++ app.rb
@@ -1,11 +1,25 @@
-require 'sinatra'
-require 'sinatra/reloader'
-also_reload 'lib/**/*.rb'
-require 'pry'
-require "pg"
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/project')
+require('./lib/volunteer')
+also_reload('lib/**/*.rb')
+require('pry')
+require('pg')
 
 DB = PG.connect({ dbname: 'volunteer_tracker', host: 'db', user: 'postgres', password: 'password' })
 
 get '/' do
-  "This is connected to the database #{DB.db}."
+  @projects = Project.all
+  erb(:main)
+end
+
+get '/projects/new' do
+  erb(:new_project)
+end
+
+post '/projects' do
+  title = params[:title]
+  project = Project.new({:title => title, :id => nil})
+  project.save()
+  redirect to '/'
 end
