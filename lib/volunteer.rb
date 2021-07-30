5912de054836088bff563e15db6071b107ee9dca
Kris Hopper <hopperdavis@gmail.com>
2021-07-30 13:08:56 -0700

    pass .find_by_project test in volunteer_spec

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -45,4 +45,17 @@ class Volunteer
     end
   end
 
+  def self.find_by_project(prj_id)
+    volunteers = []
+    returned_volunteers = DB.exec("SELECT * FROM volunteers WHERE project_id = #{prj_id};")
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => prj_id, :id => id}))
+    end
+    volunteers
+  end
+
+
+
 end
