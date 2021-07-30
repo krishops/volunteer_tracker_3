9d04ba287aa7dc418573de1ca6211f669ac4d23a
Kris Hopper <hopperdavis@gmail.com>
2021-07-30 14:27:35 -0700

    add views and routing to update and delete projects

--- app.rb
+++ app.rb
@@ -9,6 +9,10 @@ require('pg')
 DB = PG.connect({ dbname: 'volunteer_tracker', host: 'db', user: 'postgres', password: 'password' })
 
 get '/' do
+  redirect to '/projects'
+end
+
+get '/projects' do
   @projects = Project.all
   erb(:main)
 end
@@ -23,3 +27,33 @@ post '/projects' do
   project.save()
   redirect to '/'
 end
+
+get '/projects/:id' do
+  @project = Project.find(params[:id].to_i())
+  erb(:project)
+end
+
+get '/projects/:id/edit' do
+  @project = Project.find(params[:id].to_i())
+  erb(:edit_project)
+end
+
+patch '/projects/:id' do
+  @project = Project.find(params[:id].to_i())
+  @project.update(params[:title])
+  redirect to('/projects')
+end
+
+delete '/projects/:id' do
+  @project = Project.find(params[:id].to_i())
+  @project.delete()
+  redirect to('/projects')
+end
+
+post '/projects/:id/volunteers' do
+  @project = Project.find(params[:id].to_i())
+  volunteer = Volunteer.new({:name => params[:volunteer_name], :project_id => @project.id, :id => nil})
+  volunteer.save()
+  erb(:project)
+end
+
