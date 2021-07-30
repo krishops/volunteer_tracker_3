ad02c3f49099721d6855114f2b606e2b75c516b5
Kris Hopper <hopperdavis@gmail.com>
2021-07-30 11:41:22 -0700

    pass #==, fail .all & #save tests in volunteer_spec

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -8,6 +8,14 @@ class Volunteer
     @id = attributes.fetch(:id)
   end
 
+  def ==(volunteer_to_compare)
+    if volunteer_to_compare != nil
+      (self.name() == volunteer_to_compare.name()) && (self.project_id() == volunteer_to_compare.project_id())
+    else
+      false
+    end
+  end
+
   def save
     result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
     @id = result.first().fetch("id").to_i
