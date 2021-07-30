5fc12db1bcd9149a321bcd42f9b8a7f7f814b4c8
Kris Hopper <hopperdavis@gmail.com>
2021-07-30 11:06:20 -0700

    pass #title test in project_spec

---
+++ lib/project.rb
@@ -0,0 +1,10 @@
+class Project
+  attr_accessor :title
+  attr_reader :id
+  
+  def initialize(attributes)
+    @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id)
+  end
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'rspec'
 require 'pg'
 require 'pry'
+require 'project'
 
 DB = PG.connect({ dbname: 'volunteer_tracker_test', host: 'db', user: 'postgres', password: 'password' })
 
